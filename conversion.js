4557da524d63b5ff38a84d2f8e70a6d7413cd878
SakshiSharma5 <140098391+SakshiSharma5@users.noreply.github.com>
2023-11-25 16:03:34 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,25 @@
+let num = 90;
+// let convertedOne = String(num);
+// let convertedOne = Boolean(num);
+// console.log(typeof(convertedOne));
+// console.log(convertedOne);
+
+
+
+let valueToBeChanged = null;
+// let changedValue = Number(valueToBeChanged);
+// let changedValue = String(valueToBeChanged);
+// let changedValue = Boolean(valueToBeChanged);
+// console.log(typeof(changedValue));
+// console.log(changedValue);
+
+/*
+Number(null) = 0
+*/
+
+
+let UndefinedVar = undefined;
+// let changedValue = Number(UndefinedVar);
+// let changedValue = Boolean(UndefinedVar);
+// console.log(typeof(changedValue));
+// console.log(changedValue);
